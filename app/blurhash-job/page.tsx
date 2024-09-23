997faad120602734a3cacc8156ee529a277f050f
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-23 17:47:09 +0100

    fix build error

--- app/blurhash-job/page.tsx
+++ app/blurhash-job/page.tsx
@@ -3,9 +3,9 @@ import { runBlurhashJob } from "@/jobs/blurhash";
 import React from "react";
 const Blurhash = async () => {
   console.log("Running blurhash job");
-  const response = await fetch("https://freeland-pied.vercel.app/api/cron/blurhash");
-  const data = await response.json();
-  console.log("Response from blurhash job", data);
+  const isSuccess = await runBlurhashJob();
+
+  console.log("Response from blurhash job", isSuccess ? "success" : "failed");
   return <div>blurhash</div>;
 };
 
